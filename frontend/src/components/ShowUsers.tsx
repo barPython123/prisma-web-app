4e3ecee655a51f57ec1b4fd522d03e11d4ca088a
GregD <test@gmail.com>
2022-12-23 12:39:57 +0100

    working fetch to component

--- frontend/src/components/ShowUsers.tsx
+++ frontend/src/components/ShowUsers.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import User from "./User";
 
 // take Array of objects as props and for each displays different user with data
 
 function ShowUsers() {
+  const [fetchedUsers, setFetchedUsers] = useState({});
+
+  fetch("http://192.168.0.75:6020/", { mode: "cors" })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
   return (
     <div>
       <table>
