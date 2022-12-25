b5947658d10871745a3775341f38a40754eaba22
GregD <test@gmail.com>
2022-12-25 22:46:31 +0100

    added props to ShowUsers component and added map rendere for individual Users

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import CreateUser from "./components/CreateUser";
-import ShowUsers from "./components/showUsers";
+import ShowUsers from "./components/ShowUsers";
+
+const someUser = {
+  name: "Greg",
+  surname: "Dabrowski",
+  age: 10,
+  doesLikePineapple: true,
+};
+
+const allUsers = [
+  someUser,
+  { name: "Tymon", surname: "Str", age: 50, doesLikePineapple: true },
+  { name: "Iza", surname: "Bartczak", age: 18, doesLikePineapple: false },
+];
 
 function App() {
   return (
     <div>
-      <CreateUser></CreateUser>
-      <ShowUsers></ShowUsers>
+      {/* <CreateUser></CreateUser> */}
+      <ShowUsers props={allUsers}></ShowUsers>
     </div>
   );
 }
--- frontend/src/components/ShowUsers.tsx
+++ frontend/src/components/ShowUsers.tsx
@@ -3,20 +3,17 @@ import User from "./User";
 
 // take Array of objects as props and for each displays different user with data
 
-function ShowUsers() {
+function ShowUsers(props: any) {
   const [fetchedUsers, setFetchedUsers] = useState([]);
 
-  fetch("http://192.168.0.75:6020/", { mode: "cors" })
-  .then((response) => response.json())
+  // fetch("http://192.168.0.75:6020/", { mode: "cors" })
+  // .then((response) => response.json())
+
   // .then((data) => console.log(data.usersData[0])) //array of objects
-  // .then((data) => setFetchedUsers(data)) 
+  // .then((data) => setFetchedUsers(data))
   // fetch NOT_WORKING
-  const someUser = {
-    name: "Greg",
-    surname: "Dabrowski",
-    age: 10,
-    doesLikePineapple: true,
-  };
+
+  console.log(props.props[0].name);
 
   return (
     <div>
@@ -30,12 +27,16 @@ function ShowUsers() {
           </tr>
         </thead>
         <tbody>
-          <User
-            name={someUser.name}
-            surname={someUser.surname}
-            age={someUser.age}
-            doesLikePineapple={someUser.doesLikePineapple}
-          ></User>
+          {props.props.map((element: any) => {
+            return (
+              <User
+                name={element.name}
+                surname={element.surname}
+                age={element.age}
+                doesLikePineapple={element.doesLikePineapple}
+              ></User>
+            );
+          })}
         </tbody>
       </table>
     </div>
