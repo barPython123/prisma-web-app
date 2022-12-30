901b11bb6c76fc29173b734b52841f0c429e8efd
GregD <test@gmail.com>
2022-12-30 13:05:33 +0100

    added blueprints for delete & update functions and endpoints

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import { showAll } from "./queries/showAllUsers";
 import createUser from "./queries/createUser";
+import { updateUser } from "./queries/updateUser";
+import { deleteUser } from "./queries/deleteUser";
 
 const app = express();
 const PORT = 6020;
 app.use(express.json());
 app.use(cors());
+
 app.get("/", async (req, res) => {
   const date = new Date();
   console.log(`endpoint "/" reached at ${date} `);
@@ -14,7 +17,16 @@ app.get("/", async (req, res) => {
   res.json(await showAll()); //it does not await, result is empty {} body
 });
 
-// change to POST
+app.post("/update", async (req, res) => {
+  console.log("record updated");
+  res.send("record updated");
+  updateUser();
+});
+
+app.get("/delete", async (req, res) => {
+  deleteUser();
+});
+
 app.post("/create", async (req, res) => {
   console.log(`endpoint "/create" reached}`);
   console.log(req.body);
